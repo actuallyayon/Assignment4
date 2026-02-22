207d7ba4135b94130f46c875fe46214ffcc272c5
actuallyayon <actuallyayon@gmail.com>
2026-02-23 00:33:14 +0600

    current tab variable and selection of dashboard elements added

---
+++ script.js
@@ -0,0 +1,6 @@
+let currentTab = "All";
+const interviewCount = document.getElementById("interviewCount");
+const rejectedCount = document.getElementById("rejectedCount");
+const totalCount = document.getElementById("totalCount");
+const tabCount = document.getElementById("tabCount");
+const emptyState = document.getElementById("emptyState");
